408ad96b5537b783560001f4053f2a1d6a6c5ef0
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2025-10-01 23:34:48 -0400

    GroceryList updates: - added isChecked column - added put request to toggle isChecked value for grocery items

--- src/Grocery Items/groceryItems.dao.ts
+++ src/Grocery Items/groceryItems.dao.ts
@@ -4,7 +4,7 @@ import { execute } from "../services/mysql.connector";
 import { groceryItemQueries } from "./groceryItems.queries";
 
 export const createGroceryItem = async (groceryItem: GroceryItem, userId: number) => {
-    return execute<OkPacket>(groceryItemQueries.createGroceryItem, [groceryItem.name, groceryItem.qty, groceryItem.measurements_id, userId]);
+    return execute<OkPacket>(groceryItemQueries.createGroceryItem, [groceryItem.name, groceryItem.qty, groceryItem.measurements_id, groceryItem.isChecked, userId]);
 }
 
 export const readGroceryItems = async (userId: number) => {
@@ -12,9 +12,13 @@ export const readGroceryItems = async (userId: number) => {
 }
 
 export const updateGroceryItem = async (groceryItem: GroceryItem, groceryItemId: number, userId: number) => {
-    return execute<OkPacket>(groceryItemQueries.updateGroceryItem, [groceryItem.name, groceryItem.qty, groceryItem.measurements_id, groceryItemId, userId]);
+    return execute<OkPacket>(groceryItemQueries.updateGroceryItem, [groceryItem.name, groceryItem.qty, groceryItem.measurements_id, groceryItem.isChecked, groceryItemId, userId]);
 }
 
 export const deleteGroceryItem = async (groceryItemId: number, userId: number) => {
     return execute<OkPacket>(groceryItemQueries.deleteGroceryItem, [groceryItemId, userId]);
+}
+
+export const toggleIsChecked = async (isChecked: boolean, groceryItemId: number, userId: number) => {
+    return execute<OkPacket>(groceryItemQueries.toggleItemChecked, [isChecked, groceryItemId, userId]);
 }
--- src/Grocery Items/groceryItems.queries.ts
+++ src/Grocery Items/groceryItems.queries.ts
@@ -1,7 +1,8 @@
 export const groceryItemQueries = {
-    createGroceryItem: `INSERT INTO grocery_items (name, qty, measurements_id, users_id) VALUES (?, ?, ?, ?);`,
+    createGroceryItem: `INSERT INTO grocery_items (name, qty, measurements_id, isChecked, users_id) VALUES (?, ?, ?, ?, ?);`,
     //readGroceryItems: `SELECT grocery_items.id, grocery_items.name, grocery_items.qty, measurements.measurement FROM grocery_items JOIN measurements ON grocery_items.measurements_id = measurements.id WHERE grocery_items.users_id = ?;`,
-    readGroceryItems: `SELECT id, name, qty, measurements_id FROM grocery_items WHERE users_id = ?;`,
-    updateGroceryItem: `UPDATE grocery_items SET name = ?, qty = ?, measurements_id = ? WHERE id = ? AND users_id = ?;`,
+    readGroceryItems: `SELECT g.id, g.name, g.qty, m.measurement, g.isChecked FROM grocery_items g JOIN measurements m ON m.id = g.measurements_id WHERE users_id = ?;`,
+    updateGroceryItem: `UPDATE grocery_items SET name = ?, qty = ?, measurements_id = ? isChecked = ? WHERE id = ? AND users_id = ?;`,
     deleteGroceryItem: `DELETE FROM grocery_items WHERE id = ? AND users_id = ?;`,
+    toggleItemChecked: `UPDATE grocery_items SET isChecked = ? WHERE id = ? AND users_id = ?;`
 }
